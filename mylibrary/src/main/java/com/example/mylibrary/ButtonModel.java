026c9bfb34ede5a7ee212360213215b0fbbd0e1c
Manreet <gourav.mahajan@essentialdesigns.net>
2019-03-26 17:16:04 +0530

    Initial commit

---
+++ mylibrary/src/main/java/com/example/mylibrary/ButtonModel.java
@@ -0,0 +1,21 @@
+package com.example.mylibrary;
+
+public class ButtonModel {
+    String id,name;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
